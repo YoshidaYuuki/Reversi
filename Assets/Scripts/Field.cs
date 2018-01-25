7dc8fa027f8ffe7447bdfb634a7b8ab517bd5a90
zifuka <zifuka@gmail.com>
2018-01-25 15:21:06 +0900

    オセロの石のプレハブ作成。 マウスの座標から配列のインデックスを求める所まで

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// フィールド
+public class Field : MonoBehaviour {
+
+    private RectTransform rectTransform;
+    private Vector2 canvasSize;
+
+    private Logic logic;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        CanvasScaler cs = transform.parent.GetComponent<CanvasScaler>();
+        canvasSize = cs.referenceResolution;
+
+        logic = transform.Find("Logic").GetComponent<Logic>();
+    }
+	
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 p = Input.mousePosition;
+
+            Debug.Log(ToIndex(p));
+        }
+    }
+
+    // マウス座標->オセロインデックス
+    Vector2Int ToIndex( Vector3 mousePosition )
+    {
+        Vector3 p = mousePosition;
+
+        /* スクリーン->ワールド->ビューポート->Canvas変換 */
+
+        p = Camera.main.ScreenToWorldPoint(p + Camera.main.transform.forward * 10);
+        p = Camera.main.WorldToViewportPoint(p);
+        p.x = (p.x * canvasSize.x) - (canvasSize.x * 0.5f);
+        p.y = (p.y * canvasSize.y) - (canvasSize.y * 0.5f);
+
+        Vector2Int vi = new Vector2Int();
+
+        // 座標をマス目の座標へ
+        vi.x = Mathf.FloorToInt(p.x / (rectTransform.sizeDelta.x / Logic.c_sizeOfField));
+        vi.y = Mathf.FloorToInt(p.y / (rectTransform.sizeDelta.y / Logic.c_sizeOfField));
+
+        // 左上を[0, 0]になるようにインデックスを変換
+        vi.x = vi.x + (Logic.c_sizeOfField / 2);
+        vi.y = -vi.y + (Logic.c_sizeOfField / 2 - 1);
+        return vi;
+    }
+}
---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour {
+
+    [SerializeField]
+    public const int c_sizeOfField = 8;
+
+    private bool[,] fieldState = new bool[c_sizeOfField, c_sizeOfField];
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}

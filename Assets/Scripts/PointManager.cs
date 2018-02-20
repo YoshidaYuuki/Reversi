6961d7dd07449194d4f100a05cf0404b4621caf7
zifuka <zifuka@gmail.com>
2018-02-20 18:03:40 +0900

    テクスチャの数字が使えるプレハブ追加

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointManager : MonoBehaviour {
+    private int point = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void Set(int num)
+    {
+        point = num;
+    }
+
+    public int Get()
+    {
+        return point;
+    }
+}
---
+++ Assets/Scripts/Point_UI_Manager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Point_UI_Manager : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> numberPointList;
+
+    private PointManager pointManager;
+
+    // Use this for initialization
+    void Start () {
+        pointManager = GetComponent<PointManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int point = pointManager.Get();
+        int workPoint;
+
+        for(int i = 0; i < numberPointList.Count; i++)
+        {
+            workPoint = point % 10;
+
+            Point number = numberPointList[i].GetComponent<Point>();
+            number.SetNumber(workPoint);
+
+            point = point / 10;
+        }
+    }
+}
